5f49c76bfc26f492a89aa69065bb9be7951ad87f
luis hernandez <luishernandez@ucr-guest-03-10-14-10-127.wnet.ucr.edu>
2022-05-03 16:50:22 -0700

    gif

--- src/App.js
+++ src/App.js
@@ -120,15 +120,15 @@ export const StyledLogo = styled.img`
 
 export const StyledImg = styled.img`
   box-shadow: 0px 5px 11px 2px rgba(0, 0, 0, 0.7);
-  border: 4px  var(--secondary);
+
   background-color: var(--accent);
   border-radius: 100%;
-  width: 100%;
+  width: 40%;
   @media (min-width: 900px) {
-    width: 250px;
+    width: 40%;
   }
   @media (min-width: 1000px) {
-    width: 300px;
+    width: 55%;
   }
   transition: width 0.5s;
 `;
@@ -577,12 +577,15 @@ function App() {
 
 </div>
         </div>
+        <h2>Whitepaper</h2>
+        <div>dfasdf</div>
 
         
        
        
      
     </s.Screen>
+    
     
   );
 }
