6d4df06e9c2ddf5b8f69107166d15729468583a2
luis hernandez <luishernandez@luiss-MacBook-Air.local>
2022-05-06 13:44:17 -0700

    yup

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ export const StyledLogo = styled.img`
   display: block;
   margin-left: auto;
   margin-right: auto;
-  width: 38%;
+  width: 48%;
   
   @media (min-width: 767px) {
     width: 13rem;
