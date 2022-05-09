13d5c8f1a847b717e07352166f52c852c54e4c82
luis hernandez <luishernandez@luiss-MacBook-Air.local>
2022-05-09 16:00:55 -0700

    css

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,13 @@
-import { Html, PerspectiveCamera ,OrbitControls,Stats,Text} from "@react-three/drei";
+import { Html, PerspectiveCamera, OrbitControls, Stats, Text } from "@react-three/drei";
 import { angleToRadians } from "../utils/angle";
 import { extend, useThree, useLoader, useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 import React, { useRef, useMemo } from "react";
 import { Water } from "three/examples/jsm/objects/Water.js";
-import { Link, Router} from "react-router-dom";
+import { Link, Router } from "react-router-dom";
 import "./Hero.css"
+import "animate.css/animate.min.css";
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 import { useNavigate } from "react-router-dom";
 
@@ -13,19 +15,20 @@ import { useNavigate } from "react-router-dom";
 
 
 
-export default function Hero(){
+export default function Hero() {
   const navigate = useNavigate();
-    return( 
-        <>
-     
-        <div className="cta" >
-          <h1 className="gradient-text" id="yoshi">  
-            CryptoYachtClub
-          </h1>
-          <button className="buttons" onClick={() => navigate("/mint")}><span>MINT</span> <span class="emphasis">YOU'RE NFTS</span></button>
-          </div>
-  
-   
-        </>
-    )
+  return (
+    <>
+
+      <div className="cta" >
+      <AnimationOnScroll animateIn="animate__zoomInDown " animateOnce>
+        <h1 className="gradient-text" id="yoshi">
+          CryptoYachtClub
+        </h1></AnimationOnScroll>
+        <button className="buttons" onClick={() => navigate("/mint")}><span>MINT</span> <span class="emphasis">YOU'RE NFTS</span></button>
+      </div>
+
+
+    </>
+  )
 }
